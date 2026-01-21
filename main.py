c69d57d34b9e22be6352266f63aab229a81491d4
neaksina-coder <neaksina414@gmail.com>
2026-01-21 17:58:41 +0700

    added core api for integrage frontend

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from contextlib import asynccontextmanager
 from db.base import Base
 from db.session import engine
@@ -35,6 +36,20 @@ def include_router(app):
 
 def start_application():
     app = FastAPI(lifespan=lifespan)
+    app.add_middleware(
+        CORSMiddleware,
+        allow_origins=[
+            "http://localhost:5173",
+            "http://127.0.0.1:5173",
+            "http://localhost:3000",
+            "http://127.0.0.1:3000",
+            "http://localhost:8080",
+            "http://127.0.0.1:8080",
+        ],
+        allow_credentials=True,
+        allow_methods=["*"],
+        allow_headers=["*"],
+    )
     include_router(app)
     return app
 
